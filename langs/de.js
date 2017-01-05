1d07c6d84556ce0f72ea96eaf576b897711d0c35
Christoph M. Becker <cmbecker69@gmx.de>
2017-01-05 12:44:13 +0100

    Add German localization

---
+++ langs/de.js
@@ -0,0 +1,3 @@
+tinyMCE.addI18n("de.calleditor", {
+    desc: "Plugin-Aufruf einfügen/bearbeiten"
+});
---
+++ langs/de_dlg.js
@@ -0,0 +1,3 @@
+tinyMCE.addI18n("de.calleditor_dlg", {
+    title: "Plugin-Aufruf-Editor"
+});
